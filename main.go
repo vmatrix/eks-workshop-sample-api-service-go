68c586d8aadc625eb93ab45eaa71c39ecb6b1807
vmatrix <vmatrixcloud@gmail.com>
2020-09-11 16:46:16 +0800

    x

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello WWorld - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
